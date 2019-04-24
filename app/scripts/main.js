9067386b247cc5a4543e2bc90d470f71a2ef06ca
Misiek40k <comanderrr@gmail.com>
2019-04-24 23:25:42 +0200

    eslint config fix. map added (first iteration)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,6 @@
 /* eslint-disable no-undef */
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-  attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  maxZoom: 18,
-  id: 'mapbox.streets',
-  accessToken: 'your.mapbox.access.token'
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
