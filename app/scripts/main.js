cbb00533b66d0b826ef686881457684d4c7d4943
Misiek40k <comanderrr@gmail.com>
2019-04-25 22:59:51 +0200

    main.js es6 featers added

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,13 @@
-var map = L.map('mapid').setView([51.505, -0.09], 13);
-var newMarker;
+let map = L.map('mapid').setView([51.505, -0.09], 13);
+let newMarker;
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
 // responsive map size
-document.getElementById('btnMap').addEventListener('click', function () {
-  setTimeout(function () {
+document.getElementById('btnMap').addEventListener('click', () => {
+  setTimeout(() => {
     map.invalidateSize();
   }, 600);
 });
@@ -18,7 +18,7 @@ map.on('click', addMarker);
 
 function addMarker(e) {
   newMarker = new L.marker(e.latlng, {
-    draggable: 'true'
-  })
-  .addTo(map);
+      draggable: 'true'
+    })
+    .addTo(map);
 }
